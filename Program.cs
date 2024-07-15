6b64b3b8e9dcd56003833bde03ad7ec1b4cc6642
luho <luca.pasquarelli.villa@gmail.com>
2024-07-15 14:40:23 +0200

    instantaited a connection to a postgresql server hosted locally instead of using an in memory db such as in main branch

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<GastroContext>(opt =>
-    opt.UseInMemoryDatabase("GastroList"));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<GastroContext>(options =>
+    options.UseNpgsql(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
