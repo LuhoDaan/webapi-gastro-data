1668a69b3a86aae96fb85b1f21bc07bdbca9b2cb
luho <luca.pasquarelli.villa@gmail.com>
2024-07-17 16:26:24 +0200

    error random messagges

--- obj/Debug/net8.0/GastroApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/GastroApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GastroApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340797b228c30d528b1430bda3b1465860a5b2ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("GastroApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GastroApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
