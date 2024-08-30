7397abbdeeaf18c17265f647e760a0868e72f2ba
luho <luca.pasquarelli.villa@gmail.com>
2024-08-30 17:02:36 +0200

    strated to generate dto and doubts on guid and primary key autogenerated from postgres

--- GastroApi/obj/Debug/net8.0/GastroApi.AssemblyInfo.cs
+++ GastroApi/obj/Debug/net8.0/GastroApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GastroApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5b3d6f153333ab9858a1d43575190372362014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e253432ca886a39e1e6f50a1ad07be5734b6d286")]
 [assembly: System.Reflection.AssemblyProductAttribute("GastroApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GastroApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
